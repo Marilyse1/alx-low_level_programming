fa55d0ddf01a2feaa62d6b14500e1bebb0e8e0a9
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-22 15:46:47 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	/* main description */
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}

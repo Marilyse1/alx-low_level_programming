789d69c823a02d0abb8e205c36dd07353f2d0d32
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-24 15:03:38 -0700

    eer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	/* main description */
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/* main description */
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }

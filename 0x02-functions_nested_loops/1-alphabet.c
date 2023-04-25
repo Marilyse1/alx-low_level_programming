f33e16c9d59a845f024812664d47675da2ce6524
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-25 15:49:23 -0700

    er

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /* Définition de la fonction print_alphabet */
-void print_alphabet(void)/* main description */
+void print_alphabet(void)
 {
-	/* main description */
+	/* Affiche l'alphabet en minuscules suivi d'un saut de ligne */
 	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++) {
 		_putchar(c);
-
+	}
 	_putchar('\n');
 }

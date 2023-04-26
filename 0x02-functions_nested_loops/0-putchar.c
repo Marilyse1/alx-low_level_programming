8ccf39b0a2f0674e7a9bd52f8ab89de51840a6dd
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-26 00:15:49 -0700

    edc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,11 @@
 * main - Fonction principale du programme
 *
 * Description : Cette fonction est le point d'entrée
- du programme. Elle imprime
+* du programme. Elle imprime
 *               "_putchar" suivi d'une nouvelle 
-ligne en utilisant la fonction
+*ligne en utilisant la fonction
 *               "_putchar" définie dans un autre
- fichier source. Le programme
+*fichier source. Le programme
 *               retourne 0 pour indiquer une exécution réussie.
 *
 * Return: 0 si l'exécution est réussie.
@@ -26,3 +26,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
+

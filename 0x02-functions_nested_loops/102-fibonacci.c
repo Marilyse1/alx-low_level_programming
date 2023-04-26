06672355770e85077ab8dbf47e395e661689368c
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-26 01:43:36 -0700

    ed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,20 +3,21 @@
 /**
 * main - Entry point of the program
 *
-* Description: Prints the first 50 Fibonacci numbers, separated by commas and
+* Description: Prints the first 50 Fibonacci numbers,
+* separated by commas and
 *              followed by a space, and ending with a new line.
 *
 * Return: 0 (Success)
 */
 int main(void) {
-int n = 50; // Number of Fibonacci numbers to generate
-unsigned long long int a = 1, b = 2, c; // Starting values of Fibonacci sequence
+int n = 50; /* Number of Fibonacci numbers to generate */
+unsigned long long int a = 1, b = 2, c, i; /* Starting values of Fibonacci sequence */
 
-// Print the first two Fibonacci numbers
+/* Print the first two Fibonacci numbers*/
 printf("%llu, %llu, ", a, b);
 
-// Generate and print the rest of the Fibonacci numbers
-for (int i = 3; i <= n; i++) {
+/* Generate and print the rest of the Fibonacci numbers */
+for (i = 3; i <= n; i++) {
 c = a + b;
 printf("%llu", c);
 if (i < n) {
@@ -28,5 +29,5 @@ b = c;
 
 printf("\n");
 
-return 0;
+return (0);
 }

6ce3eaad0f3f7cb7c49b48cf19216e04b543952a
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-26 01:47:20 -0700

    ze

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,15 +11,15 @@
 */
 int main(void) {
 int n = 50; /* Number of Fibonacci numbers to generate */
-unsigned long long int a = 1, b = 2, c, i; /* Starting values of Fibonacci sequence */
+unsigned long int a = 1, b = 2, c, i; /* Starting values of Fibonacci sequence */
 
 /* Print the first two Fibonacci numbers*/
-printf("%llu, %llu, ", a, b);
+printf("%lu, %lu, ", a, b);
 
 /* Generate and print the rest of the Fibonacci numbers */
 for (i = 3; i <= n; i++) {
 c = a + b;
-printf("%llu", c);
+printf("%lu", c);
 if (i < n) {
 printf(", ");
 }

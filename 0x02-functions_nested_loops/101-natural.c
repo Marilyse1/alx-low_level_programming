1980f2d093b5f41ef70228d907b514af40580b49
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-04-26 00:36:51 -0700

    ed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,17 +10,17 @@
 */
 int main(void)
 {
-int i, sum = 0; // Variable to store the sum
+int i, sum = 0; /* Variable to store the sum */
 
-// Loop through all numbers below 1024
+/* Loop through all numbers below 1024 */
 for (i = 0; i < 1024; i++) {
-// Check if the current number is a multiple of 3 or 5
+/* Check if the current number is a multiple of 3 or 5 */
 if (i % 3 == 0 || i % 5 == 0) {
-sum += i; // Add the current number to the sum
+sum += i; /* Add the current number to the sum */ 
 }
 }
 
-// Print the sum
+/* Print the sum */
 printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
 
 return (0);

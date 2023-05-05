56738fb0df7916510e10b393ff836c73d71d2bd1
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-05 14:47:57 -0700

    ed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,18 @@
 #include "main.h"
 
+/**
+* _isupper - Vérifie si un caractère est 
+* une lettre majuscule en utilisant la
+* table ASCII.
+* @c: Le caractère à vérifier
+*
+* Return: 1 si le caractère est une 
+* lettre majuscule, 0 sinon.
+*/
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return 1;
+if (c >= 65 && c <= 90)
+return (1);
 else
-return 0;
+return (0);
 }

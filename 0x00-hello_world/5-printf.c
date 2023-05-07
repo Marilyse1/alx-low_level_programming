b11dd4d1937412d075a2a5a935a0e886f7906d3b
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-07 15:26:50 -0700

    ed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
 * main - Entry point
 *
 * Description: Prints a message using printf
-* with proper grammar, but the outcome is a 
+* with proper grammar, but the outcome is a
 * piece of art.
 *
 * Return: Always 0 (Success)

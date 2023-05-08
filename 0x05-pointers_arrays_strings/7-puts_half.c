545c6626a2e965017bd699872f167d3456dfdd44
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-08 01:35:09 -0700

    de

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* puts_half - prints half of a string followed by a new line.
+* @str: the string to print.
+*
+* Return: void.
+*/
+void puts_half(char *str)
+{
+int i, len, n;
+
+len = 0;
+while (str[len] != '\0')
+len++;
+
+if (len % 2 == 0)
+n = len / 2;
+else
+n = (len - 1) / 2;
+
+for (i = n; i < len; i++)
+putchar(str[i]);
+
+putchar('\n');
+}

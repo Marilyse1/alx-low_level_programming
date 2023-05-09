8bf9aed4f1ad5be6f8de058154cae7d80ee44cba
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-08 17:06:17 -0700

    ed

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - calcule et affiche la somme des diagonales d'une matrice carrée
+ * @a: pointeur vers le début de la matrice
+ * @size: taille de la matrice
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+    int i, sum1 = 0, sum2 = 0;
+
+    for (i = 0; i < size; i++) {
+        sum1 += *(a + i * size + i);
+        sum2 += *(a + i * size + (size - i - 1));
+    }
+    printf("%d, %d\n", sum1, sum2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */

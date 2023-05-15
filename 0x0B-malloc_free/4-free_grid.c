0e8ed68fc7bc58f471c3d5a0a161e584946a9ab1
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-15 15:48:39 -0700

    ed

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 
-void free_grid(int **grid, int height) {
-for (int i = 0; i < height; i++)
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
 {
 free(grid[i]);
 }

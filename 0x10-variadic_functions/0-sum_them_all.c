ca544367634cd11a9e18f5d51a2932d96dccefc9
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-25 09:54:19 -0700

    ed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,19 +14,19 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list args;   /* Liste d'arguments variables */
+va_list args;/* Liste d'arguments variables */
 unsigned int i;
 int sum = 0;
 
 if (n == 0)
 return (0);
 
-va_start(args, n);   /* Initialisation de la liste d'arguments */
+va_start(args, n);/* Initialisation de la liste d'arguments */
 
 for (i = 0; i < n; i++)
-sum += va_arg(args, int);   /* Ajoute chaque argument à la somme */
+sum += va_arg(args, int);/* Ajoute chaque argument à la somme */
 
-va_end(args);   /* Nettoyage de la liste d'arguments */
+va_end(args);/* Nettoyage de la liste d'arguments */
 
 return (sum);
 }

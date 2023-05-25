951191e6565fcd257919d9c9f337f034feb471b0
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-05-25 11:18:57 -0700

    ed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,26 +15,29 @@
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list args;  /* Declare an argument list */
+va_list args;
 unsigned int i;
 char *str;
 
-va_start(args, n);  /* Initialize the argument list */
+/* Initialize the argument list */
+va_start(args, n);
 
+/* Get the next argument of type char */
 for (i = 0; i < n; i++)
 {
-str = va_arg(args, char *);  /* Get the next argument of type char */
+str = va_arg(args, char *);
 
 if (str != NULL)
-printf("%s", str);  /* Print the string */
+printf("%s", str);
 else
-printf("(nil)");  /* Print (nil) if the string is NULL */
+printf("(nil)");
 
 if (separator != NULL && i < n - 1)
-printf("%s", separator);  // Print the separator if it is not NULL and it's not the last string
+printf("%s", separator);
+
 }
 
-printf("\n");  /* Print a new line at the end of the function */
+printf("\n");
 
-va_end(args);  /* Clean up the argument list */
+va_end(args);
 }

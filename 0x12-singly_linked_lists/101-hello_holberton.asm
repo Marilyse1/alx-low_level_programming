f1d7978ac4d89f1b35d5130705cd019366358c20
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-06-01 09:46:58 -0700

    ed

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    format db "Hello, Holberton", 0xA, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    ; Push the format string onto the stack
+    mov rdi, format
+    ; Call printf function
+    call printf
+
+    ; Exit the program
+    mov eax, 0
+    ret

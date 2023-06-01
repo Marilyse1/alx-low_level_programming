1ab9e4917db7ee8b9016abb4a92742cd621f82fc
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-06-01 09:48:15 -0700

    ed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-    format db "Hello, Holberton", 0xA, 0
+format db "Hello, Holberton", 0xA, 0
 
 section .text
-    global main
-    extern printf
+global main
+extern printf
 
 main:
-    ; Push the format string onto the stack
-    mov rdi, format
-    ; Call printf function
-    call printf
+; Push the format string onto the stack
+mov rdi, format
+; Call printf function
+call printf
 
-    ; Exit the program
-    mov eax, 0
-    ret
+; Exit the program
+mov eax, 0
+ret

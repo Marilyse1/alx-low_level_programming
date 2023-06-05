d0569dacad0ec342b20acf305a28c1b15e3d2525
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-06-05 03:29:19 -0700

    ed

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ if (*s2 == '*')
 if (*(s2 + 1) == '\0')
 return (1);
 
-if (*s1 && wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+if (*s1 && wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
 return (1);
 }
 

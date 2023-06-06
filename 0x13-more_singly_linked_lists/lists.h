e11a4f47fad8377ce40e0802a94e405c462b3498
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-06-06 01:40:36 -0700

    ed

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,14 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/* Structure for a singly-linked list of integers */
+#include <stddef.h>
+
+/**
+* struct listint_s - liste chaînée
+* @n: entier
+* @next: pointe vers le prochain nœud
+*
+* Description : structure de nœud pour une liste chaînée
+* dans le cadre du projet Holberton
+*/
 typedef struct listint_s
 {
 int n;
 struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
+/* Prototypes des fonctions */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);

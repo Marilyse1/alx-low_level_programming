c8cbea9b8e36bd344389eb0a5f8027af2d7e3f9f
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-06-06 01:00:20 -0700

    ed

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
-* listint_len - Returns the number of elements in a linked listint_t list.
+* listint_len - Returns the number
+* of elements in a linked listint_t list.
 * @h: Pointer to the head of the list.
 *
 * Return: The number of elements in the list.

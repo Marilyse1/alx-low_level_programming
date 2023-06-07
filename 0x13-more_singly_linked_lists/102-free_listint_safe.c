115019813a94fb32c2b601090f258145b4f617a9
Marilyse Sèdé AHOUANGONOU <ahouangonoumarilyse2015@gmail.com>
2023-06-07 12:01:09 -0700

    ed

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -25,7 +25,6 @@ while (current != NULL)
 {
 next = current->next;
 
-/* Vérifier si le nœud actuel a déjà été visité */
 if (current < next)
 {
 free(current);
@@ -33,14 +32,12 @@ count++;
 }
 else
 {
-/* Boucle détectée, arrêter la libération */
 break;
 }
 
-current = (next);
+current = next;
 }
 
-/* Réinitialiser la tête de liste à NULL */
 *h = NULL;
 
 return (count);
